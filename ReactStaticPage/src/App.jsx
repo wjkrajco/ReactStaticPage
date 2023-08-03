3b4f61d34218d0ba75067569af327aa71e055cd5
Billy Krajcovic <wjkrajco@ncsu.edu>
2023-08-03 09:47:09 -0400

    Finished Simple React Static Page

--- ReactStaticPage/src/App.jsx
+++ ReactStaticPage/src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react"
+import Nav from "./Nav.jsx"
+import Main from "./MainFunct.jsx"
 
-function page() {
-
-  return (
-  <div>
-      <img src={reactLogo} width="40px"/>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-)}
-
-export default page
+export default function App() {
+    return (
+        <div className="container">
+            <Nav />
+            <Main />
+        </div>
+    )
+}
